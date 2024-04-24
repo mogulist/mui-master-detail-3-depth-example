1aad2e9ce38283ed2e5c499aa6eaf03e28da7c61
임형택/프론트엔드개발팀 <herbert@ktown4u.com>
2024-04-24 16:42:17 +0900

    불필요한 패널 내용 삭제

--- src/Demo.tsx
+++ src/Demo.tsx
@@ -1,10 +1,14 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
-import Typography from '@mui/material/Typography';
-import Paper from '@mui/material/Paper';
-import Stack from '@mui/material/Stack';
-import { DataGridPro, DataGridProProps, GridColDef } from '@mui/x-data-grid-pro';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import Paper from "@mui/material/Paper";
+import Stack from "@mui/material/Stack";
+import {
+  DataGridPro,
+  DataGridProProps,
+  GridColDef,
+} from "@mui/x-data-grid-pro";
 import {
   randomCreatedDate,
   randomPrice,
@@ -15,53 +19,31 @@ import {
   randomInt,
   randomAddress,
   randomCommodity,
-} from '@mui/x-data-grid-generator';
+} from "@mui/x-data-grid-generator";
 
 function DetailPanelContent({ row: rowProp }: { row: Customer }) {
   return (
     <Stack
-      sx={{ py: 2, height: '100%', boxSizing: 'border-box' }}
+      sx={{ py: 2, height: "100%", boxSizing: "border-box" }}
       direction="column"
     >
-      <Paper sx={{ flex: 1, mx: 'auto', width: '90%', p: 1 }}>
+      <Paper sx={{ flex: 1, mx: "auto", width: "90%", p: 1 }}>
         <Stack direction="column" spacing={1} sx={{ height: 1 }}>
-          <Typography variant="h6">{`Order #${rowProp.id}`}</Typography>
-          <Grid container>
-            <Grid item md={6}>
-              <Typography variant="body2" color="textSecondary">
-                Customer information
-              </Typography>
-              <Typography variant="body1">{rowProp.customer}</Typography>
-              <Typography variant="body1">{rowProp.email}</Typography>
-            </Grid>
-            <Grid item md={6}>
-              <Typography variant="body2" align="right" color="textSecondary">
-                Shipping address
-              </Typography>
-              <Typography variant="body1" align="right">
-                {rowProp.address}
-              </Typography>
-              <Typography
-                variant="body1"
-                align="right"
-              >{`${rowProp.city}, ${rowProp.country.label}`}</Typography>
-            </Grid>
-          </Grid>
           <DataGridPro
             density="compact"
             columns={[
-              { field: 'name', headerName: 'Product', flex: 1 },
+              { field: "name", headerName: "Product", flex: 1 },
               {
-                field: 'quantity',
-                headerName: 'Quantity',
-                align: 'center',
-                type: 'number',
+                field: "quantity",
+                headerName: "Quantity",
+                align: "center",
+                type: "number",
               },
-              { field: 'unitPrice', headerName: 'Unit Price', type: 'number' },
+              { field: "unitPrice", headerName: "Unit Price", type: "number" },
               {
-                field: 'total',
-                headerName: 'Total',
-                type: 'number',
+                field: "total",
+                headerName: "Total",
+                type: "number",
                 valueGetter: (value, row) => row.quantity * row.unitPrice,
               },
             ]}
@@ -76,18 +58,18 @@ function DetailPanelContent({ row: rowProp }: { row: Customer }) {
 }
 
 const columns: GridColDef<(typeof rows)[number]>[] = [
-  { field: 'id', headerName: 'Order ID' },
-  { field: 'customer', headerName: 'Customer', width: 200 },
-  { field: 'date', type: 'date', headerName: 'Placed at' },
-  { field: 'currency', headerName: 'Currency' },
+  { field: "id", headerName: "Order ID" },
+  { field: "customer", headerName: "Customer", width: 200 },
+  { field: "date", type: "date", headerName: "Placed at" },
+  { field: "currency", headerName: "Currency" },
   {
-    field: 'total',
-    type: 'number',
-    headerName: 'Total',
+    field: "total",
+    type: "number",
+    headerName: "Total",
     valueGetter: (value, row) => {
       const subtotal = row.products.reduce(
         (acc: number, product: any) => product.unitPrice * product.quantity,
-        0,
+        0
       );
       const taxes = subtotal * 0.05;
       return subtotal + taxes;
@@ -108,7 +90,7 @@ function generateProducts() {
 const rows = [
   {
     id: 1,
-    customer: 'Matheus',
+    customer: "Matheus",
     email: randomEmail(),
     date: randomCreatedDate(),
     address: randomAddress(),
@@ -119,7 +101,7 @@ const rows = [
   },
   {
     id: 2,
-    customer: 'Olivier',
+    customer: "Olivier",
     email: randomEmail(),
     date: randomCreatedDate(),
     address: randomAddress(),
@@ -130,7 +112,7 @@ const rows = [
   },
   {
     id: 3,
-    customer: 'Flavien',
+    customer: "Flavien",
     email: randomEmail(),
     date: randomCreatedDate(),
     address: randomAddress(),
@@ -141,7 +123,7 @@ const rows = [
   },
   {
     id: 4,
-    customer: 'Danail',
+    customer: "Danail",
     email: randomEmail(),
     date: randomCreatedDate(),
     address: randomAddress(),
@@ -152,7 +134,7 @@ const rows = [
   },
   {
     id: 5,
-    customer: 'Alexandre',
+    customer: "Alexandre",
     email: randomEmail(),
     date: randomCreatedDate(),
     address: randomAddress(),
@@ -167,13 +149,13 @@ type Customer = (typeof rows)[number];
 
 export default function BasicDetailPanels() {
   const getDetailPanelContent = React.useCallback<
-    NonNullable<DataGridProProps['getDetailPanelContent']>
+    NonNullable<DataGridProProps["getDetailPanelContent"]>
   >(({ row }) => <DetailPanelContent row={row} />, []);
 
   const getDetailPanelHeight = React.useCallback(() => 400, []);
 
   return (
-    <Box sx={{ width: '100%', height: 400 }}>
+    <Box sx={{ width: "100%", height: 400 }}>
       <DataGridPro
         columns={columns}
         rows={rows}
